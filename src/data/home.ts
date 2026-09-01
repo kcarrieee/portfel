29d71a441e02f16c68782bc524daa88cac677bc2
Karina <carriestylesf4@gmail.com>
2026-09-01 20:42:58 +0300

    some updates

--- src/data/home.ts
+++ src/data/home.ts
@@ -15,17 +15,17 @@ export const links = {
 export const homeContent = {
   en: {
     firstName: 'Karina',
-    bioGreeting: 'Hey! I am',
+    bioGreeting: 'Hello! I am',
     bioGreetingRest: 'a product designer with an eye for details',
     bioRoleIntro: 'Now I design at',
     companyName: 'wildberries',
     bioRoleRest:
-      "I'm good at quickly diving into complex domains, turning complexity into clarity, and using storytelling to move ideas from concept to launch.",
+      "I'm good at quickly diving into complex domains, turning complexity into clarity, overall building 0 -> 1 products and experiences across visual design, immersive interactions and front-end engineering.",
     bioCraft:
-      "I care deeply about craft, especially animation and software development. I’m also a strong team player: I listen, support others, and look for solutions that work for everyone.",
-    bioPrevious: 'Previously, I worked at Bank Saint Petersburg and a few other companies. Check out my',
+      "I care deeply about craft, especially animation and software development. I’m also a strong team player: I listen, support others, and leave no stone unturned.",
+    bioPrevious: 'Check out my',
     resumeLabel: 'resume / cv',
-    and: 'and',
+    and: 'and links to',
     socialsLabel: 'other socials',
     moreProjectsTitle: 'More projects',
     moreProjectsSubtitle: 'Freelance / commercial / side projects',
@@ -43,16 +43,16 @@ export const homeContent = {
   ru: {
     firstName: 'Карина',
     bioGreeting: 'Привет! Я',
-    bioGreetingRest: 'дизайнер, которому важны детали',
+    bioGreetingRest: 'дизайнер, которому очень важны детали',
     bioRoleIntro: 'Сейчас работаю в',
     companyName: 'wildberries',
     bioRoleRest:
-      'Быстро погружаюсь в сложные области, превращаю сложность в ясность и довожу идею от концепции до запуска.',
+      'Быстро погружаюсь в сложные области, превращаю сложность в ясность, создаю продукты (опыт с нуля до запуска), работая на стыке дизайна, интерактива и фронтенд-разработки.',
     bioCraft:
-      'Мне очень важно качество мелочей особенно анимации и техническая реализация. А ещё я люблю работать в команде: стараюсь слушать других, поддерживать и находить решения, которые подходят всем.',
-    bioPrevious: 'Ранее работала в Банке Санкт-Петербург и ещё нескольких компаниях. Подробнее в моём',
+      'Мне очень важно качество мелочей особенно анимации и техническая реализация. А ещё я люблю работать в команде, искать вместе решения.',
+    bioPrevious: 'Подробнее обо мне в ',
     resumeLabel: 'резюме / cv',
-    and: 'и',
+    and: 'и ссылки на ',
     socialsLabel: 'остальные соцсети',
     moreProjectsTitle: 'Другие проекты',
     moreProjectsSubtitle: 'Фриланс / коммерческие / сторонние проекты',
